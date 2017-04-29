65570456c7df344df9651c6b020b3f4de0b9b89e
NadimRai <rai_nadim@yahoo.com>
2017-04-28 18:29:09 -0700

    ability to add new portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,28 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+		if @portfolio_item.save
+			redirect_to @portfolio_item
+		else
+			render :new
+		end
+	end
+
+	def show
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
+	private
+
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle,:body)
+	end
+
 end
